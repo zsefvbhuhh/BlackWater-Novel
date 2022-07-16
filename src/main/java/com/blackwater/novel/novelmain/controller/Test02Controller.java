14f5ac68e23893410413a8890a1b165aa90a4a0f
Water <1730118383@qq.com>
2022-07-16 16:39:38 +0800

    整合测试

--- src/main/java/com/blackwater/novel/novelmain/controller/Test02Controller.java
+++ src/main/java/com/blackwater/novel/novelmain/controller/Test02Controller.java
@@ -1,7 +1,6 @@
 package com.blackwater.novel.novelmain.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**

df482459ce48dfbffbaa2099222206996273234e
Water <1730118383@qq.com>
2022-07-16 14:41:33 +0800

    ignoreFileTypes整合

--- src/main/java/com/blackwater/novel/NovelApplication.java
+++ src/main/java/com/blackwater/novel/NovelApplication.java
@@ -1,13 +1,15 @@
 package com.blackwater.novel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication : 标注这个类是SpringBoot应用
 @SpringBootApplication
 public class NovelApplication {
 
 	public static void main(String[] args) {
+		//讲SpringBoot应用启动
 		SpringApplication.run(NovelApplication.class, args);
 	}
-
 }

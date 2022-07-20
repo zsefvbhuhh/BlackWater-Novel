04e232fe5a16731908fbd0deda3803bdda210ac4
Water <1730118383@qq.com>
2022-07-20 13:58:42 +0800

    调整日志输出

--- src/test/java/com/blackwater/novel/NovelApplicationTests.java
+++ src/test/java/com/blackwater/novel/NovelApplicationTests.java
@@ -1,10 +1,20 @@
 package com.blackwater.novel;
 
+import com.blackwater.novel.novelmain.mapper.Test01Mapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NovelApplicationTests {
+	@Autowired
+	Test01Mapper test01Mapper;
+
+	@Test
+	void testMapperLog(){
+		String result = test01Mapper.getTest01StringByAuthor("water");
+		System.out.println("result = " + result);
+	}
 
 	@Test
 	void contextLoads() {
